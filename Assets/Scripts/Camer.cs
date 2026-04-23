d9dd3c9df933c88563268cf5a3c2ac1890941342
Naranat-Seeaon <naranat.seea@bumail.net>
2026-04-23 16:48:53 +0700

    final??

---
+++ Assets/Scripts/Camer.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class Camer : MonoBehaviour
+{
+    private Vector3 offset = new Vector3(0f, 0f, 0f);
+    private float smoothTime = 0.25f;
+    private Vector3 velocity = Vector3.zero;
+    [SerializeField] private Transform target;
+    
+  
+    private void Update()
+    {
+        Vector3 targetPosition = transform.position + offset;
+        transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref velocity, smoothTime);
+
+    }
+}
